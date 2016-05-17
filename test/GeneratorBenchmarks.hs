e10a8c590608420f1f463cef2595583aa48514f0
Ryan Scott <ryan.gl.scott@gmail.com>
2016-05-17 12:54:40 -0400

    Make benchmarks compile with modern criterion

--- test/GeneratorBenchmarks.hs
+++ test/GeneratorBenchmarks.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
 import Criterion.Main
 import qualified Data.ByteString.Lazy as BSL
 import System.Environment
+import Data.Monoid
 import qualified Data.Text as T
 
 import Text.XML.Generator
@@ -11,12 +13,12 @@ benchElems numberOfElems = BSL.writeFile "/tmp/test.xml" (xrender doc)
 
 benchAttrs :: Int -> IO ()
 benchAttrs numberOfElems = BSL.writeFile "/tmp/test.xml" (xrender doc)
-      where doc = xelem "root" $ xattrs $ map (\s -> xattr ("key-" ++ s) (T.pack s)) (map (\i -> show i) [1..numberOfElems])
+      where doc = xelem "root" $ xattrs $ map (\s -> xattr ("key-" `mappend` s) s) (map (\i -> T.pack (show i)) [1..numberOfElems])
 
 main =
     do args <- getArgs
        case args of
          "--elems":s:[] -> benchElems (read s)
          "--attrs":s:[] -> benchAttrs (read s)
-         _ -> defaultMain (concatMap (\i -> [bench (show i ++ " elems") (benchElems i),
-                                             bench (show i ++ " attrs") (benchAttrs i)]) [1000, 10000, 100000, 1000000])
+         _ -> defaultMain (concatMap (\i -> [bench (show i ++ " elems") (whnfIO (benchElems i)),
+                                             bench (show i ++ " attrs") (whnfIO (benchAttrs i))]) [1000, 10000, 100000, 1000000])
